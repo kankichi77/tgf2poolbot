ee5f89370de1377943c0dabe0c5e5110ceaf5fbb
Kan Hayashi <kan.hayashi@gmail.com>
2021-06-15 23:20:08 +0800

    update

--- bot.php
+++ bot.php
@@ -5,14 +5,12 @@ $telegram_path = "https://api.telegram.org/bot" . $token;
 $arg = "";
 $ts = date('Y/m/d H:i:s');
 $redis = new Redis();
-
-//$f2pool_username = "kanhne";
 $f2pool_path = "https://api.f2pool.com/bitcoin/";
 
 $commands = [
-        "/hello",
-	"/f2_online",
-	"/help",
+	"status" => "/status",
+	"status_workers" => "/status_workers",
+	"help" => "/help",
 ];
 
 try {
@@ -40,52 +38,57 @@ if ($update) {
   $username = $update["message"]["from"]["username"];
   $user_id = $update["message"]["from"]["id"];
 
-//  if (strpos($message, $commands[0]) === 0) {
-  if (isCommand($message, $commands, 0)) {      
-    //$arg = substr($message, strlen($command1)+1);
-    returnTgMessage($message);	  
-    //file_get_contents($telegram_path."/sendmessage?chat_id=".$chatId."&text=Hello ".$user_id);
-    
-  } elseif (isCommand($message, $commands, 1)) {	  
-    $f2pool_username = substr($message, strlen($commands[1])+1);
-    //returnTgMessage($f2pool_username);
-  //}elseif (0) {
+  if (isCommand($message, $commands, "status")) {
+    if (isCommand($message, $commands, "status_workers")) {
+      $f2pool_username = substr($message, strlen($commands["status_workers"])+1);
+    } else {
+      $f2pool_username = substr($message, strlen($commands["status"])+1);
+    }
+    $f2pool_username = strtok($f2pool_username, " ");
+    if ($f2pool_username == "" || $f2pool_username == "username") {
+      returnTgMessage("Please specify the username");
+      exit;
+    }
     $path = $f2pool_path . $f2pool_username;
     $f2pool_info = json_decode(file_get_contents($path), TRUE);
     if (isValidF2Username($f2pool_info)) {
-      //$message = "F2 Pool Info retrieved";
-      $message = $f2pool_info["worker_length_online"] . "/" . $f2pool_info["worker_length"];
-      $message .= " worker(s) online\n";
-      $message .= "Total Current Hashrate: " . toTH($f2pool_info["hashrate"],2) . "\n";
-      $message .= "Total 24h hashrate: " . toTH($f2pool_info["hashes_last_hour"],2) . "\n";
-      $message .= "\n";
-      $counter = 0;
-      foreach ($f2pool_info["workers"] as $worker) {
-	$counter++;      
-        $message .= $counter . ") " . $worker[0] . " - " . toTH($worker[1],2) . " - " . toTH($worker[2],2);
+      $reply = $f2pool_info["worker_length_online"] . "/" . $f2pool_info["worker_length"];
+      $reply .= " worker(s) online\n";
+      $reply .= "Total Current Hashrate: " . toTH($f2pool_info["hashrate"],2) . "\n";
+      $reply .= "Total 24h hashrate: " . toTH($f2pool_info["hashes_last_day"]/(24*60*60),2) . "\n";
+
+      if (isCommand($message, $commands, "status_workers")) {
+        $reply .= "\n";
+        $reply .= "Workers:\n";
+        $counter = 0;
+        foreach ($f2pool_info["workers"] as $worker) {
+	  $counter++;      
+	  $reply .= $counter . ") " . $worker[0] . " - " . toTH($worker[1],2) . " - ";
+	  $reply .= toTH($worker[4]/(24*60*60),2);
+        }
       }
-      $message .= "";
     } else {
-      $message = "Error";
+      $reply = "Error retrieving pool data.";
     }
-    returnTgMessage($message);
-    //file_get_contents($telegram_path."/sendmessage?chat_id=".$chatId."&text=".$message);
-
-  } elseif (isCommand($message, $commands, 2)) {
-    $message = "This bot will help you retrieve stats on your miner at the F2 Pool\n";
-    $message .= "\n";
-    $message .= "/f2_online_<f2_username>\n";
-    $message .= "Replace <username> with your F2 Pool username. This will show you your current online workers' stats in the following format:\n";
-    $message .= "worker_name - last_1_hour_hashrate - last_24hour_hashrate\n";
-
-    returnTgMessage($message);
+    returnTgMessage($reply);
+  } elseif (isCommand($message, $commands, "help")) {
+    $reply = "This bot will retrieve stats on your miner at the F2 Pool\n";
+    $reply .= "\n";
+    $reply .= "/status_<username>\n";
+    $reply .= "Replace <username> with your F2 Pool username. This will show you a summary of your current mining stats.\n";
+    $reply .= "\n";
+    $reply .= "/status_workers_<username>\n";
+    $reply .= "Replace <username> with your F2 Pool username. This will show you a summary along with stats for each of your workers in the following format:\n";
+    $reply .= "worker_name - 15min_hashrate - 24hour_hashrate\n";
+    $reply .= "\n";
+    $reply .= "Note: Please keep your username private.  This bot does not store your username by default.";
+    returnTgMessage($reply);
   } else {
-    if (strlen($message) > 12) $message = substr($message, 0, 12) . "...";
-    returnTgMessage($message);
-    //file_get_contents($telegram_path."/sendmessage?chat_id=".$chatId."&text=".$message);
+    // Do nothing	  
+    // returnTgMessage($message);
   }
-} elseif (1) {
-  //echo "bot!";
+} elseif (0) {
+  // DEBUG
   echo "<html><head><title></title></head><body>";	
   echo $redis->get("log_count") . "<br>";
   echo $redis->get("previous_last_accessed") . "<br>";
@@ -95,7 +98,7 @@ if ($update) {
 
 function isValidF2Username($pool_info) {
   $result = false;
-  if ($pool_info["worker_length"] != "0") {
+  if ($pool_info["worker_length"] != "0" && $pool_info["worker_length"] != "") {
     $result = true;
   }
   return $result;
@@ -111,7 +114,15 @@ function returnTgMessage($m) {
 }
 
 function toTH($h,$d) {
-  //return floatval($h)/1000000000000;	
-  return round(floatval($h)/1000000000000,$d);
+  if ($h == "" || floatval($h) == 0) {
+    return 0;
+  }
+  return floor(floatval($h)/10000000000)/100;
+}
+
+function getNetHR($grossHR, $rejectedH) {
+  $grossHR = floatval($grossHR);
+  $rejectedH = floatval($rejectedH);
+  return $grossHR - $rejectedH;
 }
 ?>

142e31af6ef607451f187e460f03510057849317
Kan Hayashi <kan.hayashi@gmail.com>
2021-06-20 23:53:58 +0800

    add prod/dev args

--- batch.php
+++ batch.php
@@ -53,82 +53,63 @@ if (isset($argv[1])) {
 	  }
   } elseif ($argv[1] == "debug-off") {
 	  $db->setDebugModeOff();
-  }
-} else {
-  while($db->isBatchSwitchOn()) {
-	// MAIN LOOP
-	$db->setBatchRunningStatusOn();
-	$userIds = $db->getOfflineAlertOnUserIds();
+  } elseif ($argv[1] == "prod"
+	    || $argv[1] == "dev"
+  ) {
+	  while($db->isBatchSwitchOn()) {
+	  // MAIN LOOP
+	  	$db->setBatchRunningStatusOn();
+	  	$userIds = $db->getOfflineAlertOnUserIds();
 	
-	foreach ($userIds as $uid) {
-		$f2_username = $db->getF2Username($uid);
-		if ($f2_username != "") {
-			$pool->setUsername($f2_username);
-			if ($pool->fetchPoolInfo()) {
-				if($pool->numOfOfflineWorkers()
-					|| $db->isDebugModeOn()
-				) {
-					$tg->setChatId($db->getChatId($uid));
-					$tg->returnTgMessage($pool->getOfflineAlertMessage());
-					$db->setOfflineAlertOff($uid);
-				}
-			} else {
-				$tg->returnTgMessage($ERROR["CANNOT_RETRIEVE_POOL_INFO"]);
-			}
-		} else {
-			$tg->returnTgMessage($ERROR["F2_USERNAME_UNDEFINED"]);
-		}
-	}	
-
-
-    // get all f2_username where auto_monitor == on for that user
-	//$userIds = Array();
-	  //$usernames = array();	  
-	$userIds = $db->getAutoMonitorModeOnUserIds();
-	//foreach($userIds as $uid) {
-	//	$usernames[] = $db->getF2Username($uid);
-	//}
-    
-    // for each f2_username get next_batch_run_time
-	foreach ($userIds as $uid) {
-		$f2_username = $db->getF2Username($uid);
-		if ($f2_username != "") {
-			//echo "\nuser id: " . $uid . "\n";
-			//echo "f2 username: " . $f2_username . "\n";
-			$next_run_time = $db->getNextBatchRunTime($uid);
-			//echo "now: " . time() . "\n";
-			//echo "next: " . $next_run_time . "\n";
-
-			// if current_time > next_batch_run_time then run the command
-			if ($next_run_time <= time()) {
-				//$tg = new Telegram();
-				//echo "chat id: " . $db->getChatId($uid) . "\n";
-				$tg->setChatId($db->getChatId($uid));
-				// run command
+		foreach ($userIds as $uid) {
+	  		$f2_username = $db->getF2Username($uid);
+			if ($f2_username != "") {
 				$pool->setUsername($f2_username);
 				if ($pool->fetchPoolInfo()) {
-					$m = "[Auto Monitor]\n";
-					$m .= $pool->getStatusSummaryMessage();
-					$tg->returnTgMessage($m);
-					//echo "Telegram message sent.\n";
+					if($pool->numOfOfflineWorkers()
+					|| $db->isDebugModeOn()
+				  	) {
+						$tg->setChatId($db->getChatId($uid));
+						$tg->returnTgMessage($pool->getOfflineAlertMessage());
+						$db->setOfflineAlertOff($uid);
+				  	}
 				} else {
-					$m = "Error retreiving Pool Information.";
-					$tg->returnTgMessage($m);
-					//echo $m . "\n";
+					$tg->returnTgMessage($ERROR["CANNOT_RETRIEVE_POOL_INFO"]);
+			  	}
+		  	} else {
+				$tg->returnTgMessage($ERROR["F2_USERNAME_UNDEFINED"]);
+		  	}
+	  	}
+	  	// get all f2_username where auto_monitor == on for that user
+	  	$userIds = $db->getAutoMonitorModeOnUserIds();
+  	  	// for each f2_username get next_batch_run_time
+	  	foreach ($userIds as $uid) {
+			$f2_username = $db->getF2Username($uid);
+			if ($f2_username != "") {
+				$next_run_time = $db->getNextBatchRunTime($uid);
+				// if current_time > next_batch_run_time then run the command
+				if ($next_run_time <= time()) {
+					$tg->setChatId($db->getChatId($uid));
+					// run command
+					$pool->setUsername($f2_username);
+					if ($pool->fetchPoolInfo()) {
+						$m = "[Auto Monitor]\n";
+						$m .= $pool->getStatusSummaryMessage();
+						$tg->returnTgMessage($m);
+					} else {
+						$m = "Error retreiving Pool Information.";
+						$tg->returnTgMessage($m);
+					}
+					// set next_batch_run_time for that user
+					$db->setNextBatchRunTime($uid);
+				} else {
 				}
-				// set next_batch_run_time for that user
-				$db->setNextBatchRunTime($uid);
-			} else {
-				//echo "next: in " . floor(($next_run_time - time())/60) . " min ";
-			       	//echo ($next_run_time - time()) % 60 . " sec\n\n";
 			}
-		}
-	}
-
-	//var_dump($userIds);
-	//var_dump($usernames);
-    sleep($sleep_interval);
+	  	}
+		sleep($sleep_interval);
+	  }
+	  $db->setBatchRunningStatusOff();
   }
-  $db->setBatchRunningStatusOff();
+} else {
 }
 ?>
